f420ae44f15b8d4527a27a27505467615813ae50
ShootingStar <yusung910@nate.com>
2022-04-19 15:36:03 +0900

    C언어 3일차 과제

---
+++ C_Day3-2/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h> // rand() 함수
+#include <conio.h>  // getch() 함수
+#include <time.h>   // time() 함수
+
+void main()
+{
+    int  nRandom;
+
+
+    printf(">> 엔터키를 누르면 난수가 발생합니다 ( 0 ~ 7 ) << ");
+
+    char c = _getch();
+
+    printf("\n");
+    //엔터키를 눌러야 실행됨. 그외엔 실행되지 않는다.
+    if(c == 13){
+        //// 실행시 마다 다른 수를 발생시키기 위해 시간을 적용
+        //// 프로그램 실행시 최초 한번만 해준다
+        srand((unsigned)time(NULL));
+
+        //// 나머지 연산으로 0~7까지의 난수를 발생
+        nRandom = rand() % 8;
+
+        if (nRandom == 0)
+            puts("난수는 0입니다");
+        else if (nRandom == 1)
+            puts("난수는 1입니다");
+        else if (nRandom == 2)
+            puts("난수는 2입니다");
+        else if (nRandom == 3)
+            puts("난수는 3입니다");
+        else if (nRandom >= 4 && nRandom <= 5)
+            puts("난수는 4 ~ 5입니다");
+        else
+            puts("난수는 6 ~ 7입니다");
+    }
+}
--- C_Day3/main.c
+++ C_Day3/main.c
@@ -2,7 +2,62 @@
 
 void main()
 {
+    float flA = 0;
+    float flB = 0;
+    char chrSign = '\0';
+    float flRslt = 0;
 
+    printf("계산식을 입력하여주세요 : ");
+    scanf("%f %c %f", &flA, &chrSign, &flB);
+    //과제 1-1
+    //if (chrSign == '+')
+    //{
+    //    flRslt = flA + flB;
+    //    printf("계산식 결과는 %.5f입니다.", flRslt);
+    //}
+    //else if(chrSign == '-')
+    //{
+    //    flRslt = flA - flB;
+    //    printf("계산식 결과는 %.5f입니다.", flRslt);
+    //}
+    //else if (chrSign == '*')
+    //{
+    //    flRslt = flA * flB;
+    //    printf("계산식 결과는 %.5f입니다.", flRslt);
+    //}
+    //else if (chrSign == '/')
+    //{
+    //    flRslt = flA * flB;
+    //    printf("계산식 결과는 %.5f입니다.", flRslt);
+    //}
+    //else
+    //{
+    //    puts("유효하지 않은 계산식입니다.");
+    //}
+
+    //과제 1-2
+    switch(chrSign){
+        case '+':
+            flRslt = flA + flB;
+            printf("계산식 결과는 %.5f입니다.", flRslt);
+            break;
+
+        case '-':
+            flRslt = flA - flB;
+            printf("계산식 결과는 %.5f입니다.", flRslt);
+            break;
+        case '*':
+            flRslt = flA * flB;
+            printf("계산식 결과는 %.5f입니다.", flRslt);
+            break;
+        case '/':
+            flRslt = flA * flB;
+            printf("계산식 결과는 %.5f입니다.", flRslt);
+            break;
+        default:
+            puts("유효하지 않은 계산식입니다.");
+            break;
+    }
 
 }
 
