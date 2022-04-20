926fc9225b1683ecbe3be9a2097204795668a1eb
ysmma910@gmail.com <ysmma910@gmail.com>
2022-04-20 14:01:32 +0900

    원복

--- C_Day3/main.c
+++ C_Day3/main.c
@@ -7,8 +7,6 @@ void main()
     char chrSign = '\0';
     float flRslt = 0;
 
-
-
     printf("계산식(+,-,*,/)을 입력하여주세요 : ");
     scanf("%f %c %f", &flA, &chrSign, &flB);
     //과제 1-1
@@ -37,31 +35,28 @@ void main()
         puts("유효하지 않은 계산식입니다.");
     }
 
+	//과제 1-2
+	//switch (chrSign) {
+	//case '+':
+	//	flRslt = flA + flB;
+	//	printf("계산식 결과는 %.5f입니다.", flRslt);
+	//	break;
 
-
-    //과제 1-2
-    //switch(chrSign){
-    //    case '+':
-    //        flRslt = flA + flB;
-    //        printf("계산식 결과는 %.5f입니다.", flRslt);
-    //        break;
-
-    //    case '-':
-    //        flRslt = flA - flB;
-    //        printf("계산식 결과는 %.5f입니다.", flRslt);
-    //        break;
-    //    case '*':
-    //        flRslt = flA * flB;
-    //        printf("계산식 결과는 %.5f입니다.", flRslt);
-    //        break;
-    //    case '/':
-    //        flRslt = flA * flB;
-    //        printf("계산식 결과는 %.5f입니다.", flRslt);
-    //        break;
-    //    default:
-    //        puts("유효하지 않은 계산식입니다.");
-    //        break;
-    //}
-
+	//case '-':
+	//	flRslt = flA - flB;
+	//	printf("계산식 결과는 %.5f입니다.", flRslt);
+	//	break;
+	//case '*':
+	//	flRslt = flA * flB;
+	//	printf("계산식 결과는 %.5f입니다.", flRslt);
+	//	break;
+	//case '/':
+	//	flRslt = flA * flB;
+	//	printf("계산식 결과는 %.5f입니다.", flRslt);
+	//	break;
+	//default:
+	//	puts("유효하지 않은 계산식입니다.");
+	//	break;
+	//}
 }
 
