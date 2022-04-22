4dbf27ffe760a22a355a40c32bc0e61631b43612
ysmma910@gmail.com <ysmma910@gmail.com>
2022-04-22 14:26:39 +0900

    구구단 출력 소스 일부 수정

--- C_Day4/main.c
+++ C_Day4/main.c
@@ -3,19 +3,23 @@
 void main(){
 
 	//구구단 출력 과제
+
+	//메뉴 번호 저장 변수
 	int nUserDat = 0;
+	//출력할 구구단 변수
 	int nOutNum = 0;
+
 	while (1) {
 
 		puts("원하시는 메뉴를 입력하세요.");
-		puts("1. 구구단 모두 출력, 2. 원하는 구구단 출력. 3. 종료 : ");
+		printf("1. 구구단 모두 출력, 2. 원하는 구구단 출력. 3. 종료 : ");
 		scanf("%d", &nUserDat);
 
 		switch (nUserDat) {
 		case 1:
 			//반복문 중첩 실행
 			for (int i = 1; i <= 9; i++) {
-				for (int j = 1; j <= 9; j++) {
+				for (int j = 2; j <= 9; j++) {
 					printf("%d X %d = %d  ", j, i, i*j);
 				}
 				printf("\n");
