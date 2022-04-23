f253e27b392e707326c4a7e854fc4885992ac40f
ShootingStar <yusung910@nate.com>
2022-04-23 21:57:23 +0900

    슬롯머신 게임 제작

--- C_Day4-2/main.c
+++ C_Day4-2/main.c
@@ -10,18 +10,56 @@ void main() {
 	int nRepeat = 0;
 	//슬롯머신 결과 3개의 숫자값이 저장될 변수.
 	int nA, nB, nC;
+    //유저 점수
+    unsigned int nInitScore = 10;
+    //슬롯머신 결과에 따라 변경될 점수를 저장하는 변수.
+    int nModifyScore = 0;
 
 	//시간 값을 이용하여 난수 세팅
 	srand((unsigned)time(NULL));
-	
-	puts("----슬롯머신----");
-	printf("반복 횟수를 입력하시면 게임이 시작됩니다:");
-	scanf("%d", &nRepeat);
-	while(1) {
-		nA = (rand() % 9) + 1;
-		nB = (rand() % 9) + 1;
-		nC = (rand() % 9) + 1;
-		printf("\r[%d] [%d] [%d]", nA, nB, nC);
-		_sleep(100);
-	}
+
+    while(1){
+        nModifyScore = -5;
+        printf("\r현재점수 : %d\n", nInitScore);
+        printf("\r반복 횟수를 입력하시면 게임이 시작됩니다:");
+        scanf("%d", &nRepeat);
+
+        for (int i = 0; i < nRepeat; i++) {
+            nA = rand() % 9;
+            nB = rand() % 9;
+            nC = rand() % 9;
+            printf("\r[%d] [%d] [%d]", nA, nB, nC);
+            _sleep(100);
+        }
+
+        printf("\n");
+
+        //점수 3개 중 두개가 같으면 10점을 더한다.
+        if (nA == nB || nB == nC || nA == nC) {
+            nModifyScore = 10;
+        }
+        //점수 3개가 다 같으면 50점을 더한다.
+        if (nA == nB && nB == nC && nA == nC) {
+            nModifyScore = 50;
+        }
+
+        if (nA == 7 && nB == 7 && nC == 7) {
+            nModifyScore = 1000;
+        }
+
+        printf("획득 점수 : %d\n", nModifyScore);
+        nInitScore = nInitScore + nModifyScore;
+
+        if (nInitScore == 0) {
+            puts("모든 점수를 잃어버렸습니다!");
+            printf("다시하시겠습니까?(Enter Key를 눌러주세요)");
+            char cInputKey = _getch();
+            if (cInputKey == 13) {
+                system("cls");
+                nInitScore = 10;
+            }else {
+                break;
+            }
+        }
+    }
 }
