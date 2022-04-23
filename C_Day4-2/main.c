021bb05da4be6efe00e0cda7cc244f188bcf5810
ShootingStar <yusung910@nate.com>
2022-04-24 01:56:14 +0900

    슬롯머신 게임 수정

--- C_Day4-2/main.c
+++ C_Day4-2/main.c
@@ -9,7 +9,7 @@ void main() {
 	//사용자가 입력한 반복횟수 저장 변수
 	int nRepeat = 0;
 	//슬롯머신 결과 3개의 숫자값이 저장될 변수.
-	int nA, nB, nC;
+	int nA = 7, nB = 7, nC=7;
     //유저 점수
     unsigned int nInitScore = 10;
     //슬롯머신 결과에 따라 변경될 점수를 저장하는 변수.
@@ -18,11 +18,23 @@ void main() {
 	//시간 값을 이용하여 난수 세팅
 	srand((unsigned)time(NULL));
 
+    printf("[%d] [%d] [%d]\n", nA, nB, nC);
+
     while(1){
+
+        printf("획득 점수 : %d\n", nModifyScore);
+
+        printf("현재 점수 : %d\n", nInitScore);
+
         nModifyScore = -5;
-        printf("\r현재점수 : %d\n", nInitScore);
-        printf("\r반복 횟수를 입력하시면 게임이 시작됩니다:");
+
+        printf("반복 횟수를 입력하시면 게임이 시작됩니다:");
         scanf("%d", &nRepeat);
+
+        
+        if (nA != -1 && nB != -1 && nC != -1) {
+            system("cls");
+        }
 
         for (int i = 0; i < nRepeat; i++) {
             nA = rand() % 9;
@@ -47,7 +59,6 @@ void main() {
             nModifyScore = 1000;
         }
 
-        printf("획득 점수 : %d\n", nModifyScore);
         nInitScore = nInitScore + nModifyScore;
 
         if (nInitScore == 0) {
@@ -57,6 +68,8 @@ void main() {
             if (cInputKey == 13) {
                 system("cls");
                 nInitScore = 10;
+                nModifyScore = 5;
+                printf("[7] [7] [7]\n");
             }else {
                 break;
             }
