5a88fc972b4d4e602eaf1cf541f0455a4d30fed8
ShootingStar <yusung910@nate.com>
2022-04-26 17:01:35 +0900

    구구단 출력 과제 일부 수정

--- C_Day5-1/main.c
+++ C_Day5-1/main.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 #include "Display.h";
 int nInput;
 
@@ -6,5 +6,5 @@ void main()
 {
     printf("출력할 구구단 숫자를 입력하여주세요.");
     scanf("%d", &nInput);
-    printf("마지막의 전 결과 값은 %d 입니다.", Display());
+    printf("%d단의 마지막 전 값은 %d 입니다.", nInput, Display());
 }
