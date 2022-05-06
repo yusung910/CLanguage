00ae6faa57f571ef13554c136b00503cb4f3acb8
ShootingStar <yusung910@nate.com>
2022-05-06 19:36:40 +0900

    작업 완료

--- C_Day7/__randmem.c
+++ C_Day7/__randmem.c
@@ -16,7 +16,7 @@ unsigned char* __randmem(int nSize) {
 	//개행문자 갯수
 	int nEnter = (chrSize / 4) / 10;
 	//생성할 배열의 사이즈
-	int memSize = chrSize + nEnter;
+	int memSize = chrSize + nEnter + (sizeof(char));
 	//메모리 동적 할당
 	retMem = (unsigned char*)malloc(memSize);
 
@@ -32,29 +32,35 @@ unsigned char* __randmem(int nSize) {
 	//65 ~ 90사이의 난수를 생성하여
 	//char 변수로 캐스팅한다
 	//0 : 0 ~ 40
-	//1 : 40 ~ 80
-	//2 : 80 ~ 120
+	//1 : 41 ~ 82
+	//2 : 83 ~ 124
+    //3 : 125 ~ 166
+    //4 : 207 ~ 248
+    //5 : 249 ~ 290
+    //6 : 330 ~ 371
 	//0~39  : 문자
 	//40    : 개행문자
-	//41~81 : 문자
+	//41~82 : 문자
 	//82    : 개행문자
 	//개행숫자만큼 반복
+    //#define STD_CHR_CNT 40 
+    int addedEnterCnt = 0;
+    int nStart = 0;
+    int nEnd = 40;
 	for (int a = 0; a < nEnter; a++) {
-		int nStart = a * 40;
-		int nEnd = nEnter * 40;
+        int nChrCnt = 0;
+        for (int b = nStart; b < nEnd; b++) {
+            if (nChrCnt % 4 == 0) {
+                nRd = (rand() % 25) + 65;
+            }
+            nChrCnt += 1;
+            retMem[b] = (char)nRd;
+        }
+        retMem[nEnd] = '\n';
+        nStart = nStart + STD_CHR_CNT + 1;
+        nEnd = nEnd + STD_CHR_CNT + 1;
 	}
-	for (int i = 0; i < memSize; i++) {
-		if (i % 4 == 0) {
-			nRd = (rand() % 25) + 65;
-		}
-
-		retMem[i] = (char)nRd;
-		if (i % 40 == 0 && i != 0) {
-			retMem[i] = '\n';
-		}
-		
-	}
-	retMem[memSize] = '\0';
+	retMem[memSize-1] = '\0';
 
 	return retMem;
 }
--- C_Day7/__randmem.h
+++ C_Day7/__randmem.h
@@ -1,2 +1,3 @@
 //임의의 문자열을 4의 배수로 할당한다.
+#define STD_CHR_CNT 40
 unsigned char* __randmem(int nSize);
