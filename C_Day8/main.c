a1a153d27435c4dd37e465a2e803ad234b3b97a3
ShootingStar from Academy <ysmma910@gmail.com>
2022-05-12 15:51:54 +0900

    데미지 확률작업 시작

--- C_Day8/main.c
+++ C_Day8/main.c
@@ -2,15 +2,16 @@
 #include <stdlib.h> // rand() 함수
 #include <time.h>   // time() 함수
 #include <malloc.h>
+#include <windows.h>
 
 #define TRUE	1
 #define FALSE	0
 
-typedef int	BOOL;
+//typedef int	BOOL;
 
 typedef struct
 {
-    BOOL bIsLive;  // 생존 여부
+//    BOOL bIsLive;  // 생존 여부
     int	nDamage;      // 공격 받은 정도
     int	nLife;        // 생명
 } CHARACTER;
@@ -31,12 +32,12 @@ void main() {
 		players[i]->nLife = (rand() % 51) + 50;
 		players[i]->nDamage = 0;
     }
-	
+	printf("Enter Key(시작), Esc 키(종료)\n");
     while (1) {
-        printf("Enter Key를 눌러 Turn을 진행시켜주세요.\n");
-		if(getch() == 13){
-            
+		int inputKeyVal = getch();
+		if(inputKeyVal == 13){
             system("cls");
+			printf("Enter Key를 눌러 Turn을 진행시켜주세요, Esc키를 누를 경우 게임이 종료됩니다.\n");
 			nTurn += 1;
 			printf("Turn : %d\n", nTurn);
 			//사망자 명단 저장 배열
@@ -51,16 +52,26 @@ void main() {
 			for (int j = 0; j < 100; j++) {
 				if (players[j] != NULL) {
                     //모든 player들에게 임의의 데미지를 부여한다.
-					int nDmg = (rand() % 11);
+					//매 턴이 반복될 수록 높은 데미지를 입을 수 있도록 난이도를 상향 평준화 한다.
+					int nFlagDmg = (rand() % 100);
+
+					int nDmgProbList[] = { 0.05, 0.05, 0.1, 0.1, 0.2, 0.1, 0.1, 0.1, 0.1, 0.1 };
+					int nDmg = 0;
+
+					for (int i = 0; i < 10; i++) {
+						//0: 0 ~ 4, 0.05
+						//1: 5 ~ 10, 0.05
+						//2: 11 ~ 20, 0.1
+						//3: 21 ~ 30, 0.1
+						//4: 31 ~ 40, 0.1
+						
+					}
+
+
 					int userLife = players[j]->nLife;
 
 					players[j]->nDamage = nDmg;
 					players[j]->nLife = userLife - nDmg;
-
-                    //회피데미지가 발생 할 경우 카운팅
-					if (nDmg == 0) {
-						nDodge += 1;
-					}
 
                     if (players[j]->nLife <= 0) {
                         //데미지를 입은 유저를 사망처리
@@ -104,29 +115,54 @@ void main() {
                 players[nDeathNote[nPlayer]]->nLife = (rand() % 51) + 50;
                 players[nDeathNote[nPlayer]]->nDamage = 0;
             }
-
+			printf("User / Live / Life / Dmg , User / Live / Life / Dmg , User / Live / Life / Dmg , User / Live / Life / Dmg , User / Live / Life / Dmg\n");
 			//결과 출력
 			for (int b = 0; b < 100; b++) {
 
-				char* heartFlag = (players[b] == NULL) ? "\u2665" : "\u2661";
+				char* heartFlag = (players[b] == NULL) ? "\u2661" : "\u2665";
                 int nPlayerLife = (players[b] == NULL) ? 0 : players[b]->nLife;
                 int nPlayerDamaged = (players[b] == NULL) ? 0 : players[b]->nDamage;
+				int nTextColor = (players[b] == NULL) ? 0 : 3;
 
-				printf("%3d / %s / %3d / %3d , ", b, heartFlag, nPlayerLife, nPlayerDamaged);
+				//생존한 유저 중 데미지를 0 입었을 경우 노란색으로 지정
+				if (nTextColor == 3) {
+					switch (players[b]->nDamage) {
+						case 0:
+							//데미지를 회피 했을 경우.
+							nTextColor = 6;
+							break;
+						case 10:
+							//맥스데미지(크리티컬히트)가 터졌을 경우
+							nTextColor = 4;
+					}
+					
+				}
+
+				//회피데미지가 발생 할 경우 카운팅
+				if (players[b] != NULL ){
+					if(players[b]->nDamage == 0) nDodge += 1;
+				}
+
+				//사망했을 경우 색상 변경
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), nTextColor);
+
+				printf("%4d / %4s / %4d / %3d , ", (b + 1), heartFlag, nPlayerLife, nPlayerDamaged);
 				if (b % 5 == 4) {
 					printf("\n");
 				}
 			}
 
-            //사망한 플레이어의 목록이 저장된 데이터 배열을 해제한다.
+            //사망한 플레이어의 목록이 저장된 데이터 배열을 해제한다,
+			//배열을 메모리에서 해재함으로 써 기존의 데이터를 초기화한다.
             free(nDeathNote);
 
 			printf("\n");
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
 			printf("\r생존 캐릭터 %d명 사망 캐릭터 %d명\n", (100- nDeathNoteArry), nDeathNoteArry);
 			printf("\r부활 캐릭터 %d명\n", nResurection);
 			printf("\r이번 공격으로 죽은 캐릭터 %d명\n", nDeathNum);
 			printf("\r이번 공격에 공격 받지 않은 캐릭터 %d명\n", nDodge);
-		}else {
+		}else if(inputKeyVal == 27){
 			puts("game 종료!");
 			break;
 		}
