1e9dd7604324af5ede215c9f7d375c314969741d
ShootingStar from Academy <ysmma910@gmail.com>
2022-05-16 15:34:59 +0900

    커밋

--- C_Day9/initCharacter.c
+++ C_Day9/initCharacter.c
@@ -1,8 +1,7 @@
 #include "characterStruct.h"
 #include "initCharacter.h"
 
-void initCharacter(CHARACTER* charList[100], int initCnt) {
-
+void initCharacter(CHARACTER** charList, int initCnt) {
 	for (int i = 0; i < initCnt; i++) {
 		charList[i] = (CHARACTER*)malloc(sizeof(CHARACTER));
 		charList[i]->nLife = (rand() % 51) + 50;
