94756ada1885e3304715e92675048664d3b76c77
ShootingStar from Academy <ysmma910@gmail.com>
2022-05-24 15:49:49 +0900

    C++ 과제 추가

---
+++ CPP_Day1/CPP_Day1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+namespace Calculator {
+
+	int sum(int a, int b) {
+		return a + b;
+	}
+
+	int mul(int a, int b) {
+		return a * b;
+	}
+
+	int min(int a, int b) {
+		return a - b;
+	}
+
+	float division(int a, int b) {
+		return (float) a / b;
+	}
+}
+
+using namespace Calculator;
+
+int main()
+{
+    cout << "Sum :: " << sum(10, 11) << endl;
+	cout << "mul :: " << mul(10, 11) << endl;
+	cout << "min :: " << min(10, 11) << endl;
+	cout << "div :: " << division(10, 11) << endl;
+}
