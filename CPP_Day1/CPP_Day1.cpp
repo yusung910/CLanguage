f797659d9b9739e506c33fefb69307a929eadfef
ShootingStar from Academy <ysmma910@gmail.com>
2022-05-25 14:45:06 +0900

    과제 커밋

--- CPP_Day1/CPP_Day1.cpp
+++ CPP_Day1/CPP_Day1.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 struct playersInfo {
 	int hp;
-	char name[100];
-};
+	char name[1000];
+} pinfo = {100, "test1"};
 
 
 namespace Calculator {
@@ -26,10 +26,10 @@ namespace Calculator {
 		return (float) a / b;
 	}
 
-	void initCharInfo(playersInfo pinfo) {
-		pinfo.hp = 100;
-		//pinfo.name = "플레이어1";
+	void initCharInfo(playersInfo& pinfo) {
 
+		pinfo.hp = 100;
+		strcpy(pinfo.name, "플레이어1");
 	}
 }
 
@@ -38,9 +38,12 @@ using namespace Calculator;
 int main()
 {
 
-    cout << "Sum :: " << sum(10, 11) << endl;
-	cout << "mul :: " << mul(10, 11) << endl;
-	cout << "min :: " << min(10, 11) << endl;
-	cout << "div :: " << division(10, 11) << endl;
+ //   cout << "Sum :: " << sum(10, 11) << endl;
+	//cout << "mul :: " << mul(10, 11) << endl;
+	//cout << "min :: " << min(10, 11) << endl;
+	//cout << "div :: " << division(10, 11) << endl;
+	initCharInfo(pinfo);
+	printf("hp: %d\n", pinfo.hp);
+	printf("name: %s", pinfo.name);
     return 0;
 }
