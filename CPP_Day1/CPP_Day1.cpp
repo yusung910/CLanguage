760839b93013a8196f437387ca83e5e9ed50a639
ShootingStar from Academy <ysmma910@gmail.com>
2022-05-25 15:35:49 +0900

    overloaded 과제

--- CPP_Day1/CPP_Day1.cpp
+++ CPP_Day1/CPP_Day1.cpp
@@ -14,6 +14,11 @@ namespace Calculator {
 		return a + b;
 	}
 
+	float sum(float a, float b, int c) {
+		int leng = (int) pow(10, c);
+		return round(( a + b ) * leng) / leng;
+	}
+
 	int mul(int a, int b) {
 		return a * b;
 	}
@@ -31,14 +36,16 @@ namespace Calculator {
 		pinfo.hp = 100;
 		strcpy(pinfo.name, "플레이어1");
 	}
+
+
 }
 
 using namespace Calculator;
 
 int main()
 {
-
- //   cout << "Sum :: " << sum(10, 11) << endl;
+    cout << "Sum :: " << sum(10, 11) << endl;
+	cout << "Sum :: " << sum(1.15132F, 4.2321F, 5) << endl;
 	//cout << "mul :: " << mul(10, 11) << endl;
 	//cout << "min :: " << min(10, 11) << endl;
 	//cout << "div :: " << division(10, 11) << endl;
